a9c60fb8e5c5a10258c7ec5f159ef630842d1738
wasim-khans <58463007+wasim-khans@users.noreply.github.com>
2026-02-06 16:15:06 +0000

    homework exercise

---
+++ week3/homeworkscript.js
@@ -0,0 +1,11 @@
+const textElement = document.getElementById("myText");
+
+function makeRed() {
+  textElement.style['color'] = "red";
+}
+
+function makeBlue() {
+  textElement.style.color = "blue";
+}
+// just two different ways to access the css properties, I learnt in my web design 2 class
+
