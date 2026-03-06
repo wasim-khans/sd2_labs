3dc374a99f146bd03d35b861063977a0a708ac4a
wasim-khans <58463007+wasim-khans@users.noreply.github.com>
2026-03-06 17:22:39 +0000

    Initial commit: Express app with Docker setup and database connection

---
+++ app/app.js
@@ -0,0 +1,59 @@
+// Import express.js
+const express = require("express");
+
+// Create express app
+var app = express();
+app.set("view engine", "pug");
+app.set("views", "./app/views");
+// Add static files location
+app.use(express.static("static"));
+
+// Get the functions in the db.js file to use
+const db = require('./services/db');
+
+// Create a route for root - /
+app.get("/", function(req, res) {
+    res.render("index");
+});
+
+// Create a route for testing the db
+app.get("/db_test", function(req, res) {
+    // First check if table exists and get data
+    sql = 'SELECT * FROM test_table';
+    db.query(sql).then(results => {
+        console.log(results);
+        res.json({
+            success: true,
+            data: results,
+            count: results.length
+        });
+    }).catch(error => {
+        console.error('Database error:', error);
+        res.status(500).json({
+            success: false,
+            error: error.message
+        });
+    });
+});
+
+// Create a route for /goodbye
+// Responds to a 'GET' request
+app.get("/goodbye", function(req, res) {
+    res.send("Goodbye world!");
+});
+
+// Create a dynamic route for /hello/<name>, where name is any value provided by user
+// At the end of the URL
+// Responds to a 'GET' request
+app.get("/hello/:name", function(req, res) {
+    // req.params contains any parameters in the request
+    // We can examine it in the console for debugging purposes
+    console.log(req.params);
+    //  Retrieve the 'name' parameter and use it in a dynamically generated page
+    res.send("Hello " + req.params.name);
+});
+
+// Start server on port 3000
+app.listen(3000,function(){
+    console.log(`Server running at http://127.0.0.1:3000/`);
+});
